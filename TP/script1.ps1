e39b9a0e6d85af32d7f8de8134ad2d609eff4c74
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2020-12-15 08:26:15 +0100

    Add files via upload

---
+++ TP/script1.ps1
@@ -0,0 +1,10 @@
+﻿$val= Read-Host "Tapez le nom de l'utilisateur à créer et à modifier(Si l'utilisateur existe déjà vous serez emmenés à juste le modifier)" 
+New-LocalUser $val 
+Rename-LocalUser -name $val 
+ 
+$vale= Read-Host "Tapez le nom de l'utilisateur à supprimer"
+Remove-LocalUser -Name $vale
+
+Get-LocalUser
+$valeu=Read-Host "Tapez le nom de l'utilisateur à trouver"
+Get-LocalUser -Name $valeu
